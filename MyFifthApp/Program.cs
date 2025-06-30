fd1781ed3fb38bd57f0c627e1ed78ba531234996
Kevin13Wu <89596257+Kevin13Wu@users.noreply.github.com>
2025-06-29 21:34:37 -0400

    update app on main

--- MyFifthApp/Program.cs
+++ MyFifthApp/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+System.Console.WriteLine("Welcome to the Mars!");
